311cdbe35dd936fe4875ee7f29aa6d9bfb4e4a64
saurabh <saurabhmehta1601@gmail.com>
2023-01-10 17:50:46 +0530

    feat: complete page layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,57 @@
+import React, { useState } from "react"
+import ChatSection from "./components/ChatSection"
 import Header from "./components/Header"
+import PromptSubmitForm from "./components/PromptSubmitForm"
+import axios from "axios"
+import uuid from "react-uuid"
+
+interface IMessage {
+  id: string
+  sender: "bot" | "user"
+  text: string
+}
 
 function App() {
+  const [prompt, setPrompt] = useState('')
+  const [messages, setMessages] = useState<IMessage[]>([
+    { id: "1", sender: "bot", text: "Hi how can I help you ? " },
+    { id: "2", sender: "user", text: "How can I get job at google " },
+    { id: "3", sender: "bot", text: "This is not so easy , but are you competitive programmer ? If yes it can be helpful since you must be good at data structures and algorithms which is very important" }
+  ])
+
+  const handlePromptSubmission = async (e: React.FormEvent) => {
+    e.preventDefault()
+    if (prompt.trim() === "") return
+
+    try {
+
+      const userMessageId = uuid()
+      setMessages(messages => [...messages, { id: userMessageId, sender: "user", text: prompt }])
+
+      const completionResponse = await axios.post('http://localhost:8000/get-completion', { prompt }, {
+        headers: {
+          'Content-Type': "application/json"
+        }
+      })
+      const { id, autoComplete } = completionResponse.data
+      setMessages(messages => [...messages, { id, sender: "bot", text: autoComplete }])
+    } catch (error) {
+
+    }
+
+  }
   return (
-    <div className="h-screen max-w-3xl mx-auto flex flex-col bg-purple-600 ">
+    <div className="h-screen max-w-3xl mx-auto flex flex-col ">
       <Header />
+      <ChatSection messages={messages} />
+      <PromptSubmitForm
+        prompt={prompt}
+        setPrompt={setPrompt}
+        onSubmit={handlePromptSubmission}
+      />
+    </div>
 
-      <main
-        className=" max-w-2xl bg-slate-800 rounded-md flex-1 flex flex-col mb-4 self-center w-full"
-      >
-        <section className="min-h-[200px] p-4 text-white flex-1 ">
-          <div className="w-full rounded-sm bg-slate-900 flex-1">this is chat section</div>
-        </section>
-        <form action="" className="flex bg-white rounded-b-md">
-          <input type="text" className="w-full outline-none p-2 bg-transparent" />
-          <button className="p-2">
-            <img src="/assets/icon-send-message.png" width={32} height={32} />
-          </button>
-
-        </form>
-      </main>
-    </div>)
+  )
 }
 
 export default App
