c71f5e4b863b30aa872162319cd8e03bec4a02f6
saurabh <saurabhmehta1601@gmail.com>
2023-01-10 17:17:40 +0530

    feat: add Prompt Submit Form to encapsule form submission and message update logic

---
+++ frontend/src/components/PromptSubmitForm.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+interface IProps {
+    prompt: string,
+    setPrompt: React.Dispatch<React.SetStateAction<string>>
+    onSubmit: (e: React.FormEvent) => void
+}
+
+const PromptSubmitForm = (props: IProps) => {
+    return (
+        <form
+            onSubmit={(e) => props.onSubmit(e)}
+            action="" className="flex bg-white rounded-md w-full">
+            <input
+                type="text"
+                name="prompt"
+                value={props.prompt}
+                onChange={(e) => props.setPrompt(e.target.value)}
+                className="w-full outline-none p-2 bg-transparent "
+            />
+            <button
+                className="p-2"
+                type='submit'
+                name="submit"
+            >
+                <img src="/assets/icon-send-message.png" width={32} height={32} />
+            </button>
+
+        </form>
+    )
+}
+
+export default PromptSubmitForm
