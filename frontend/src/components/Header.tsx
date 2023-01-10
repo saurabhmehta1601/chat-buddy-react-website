b70ecb54667510deef295a0da09d4ce42adb5f61
saurabh <saurabhmehta1601@gmail.com>
2023-01-10 21:42:47 +0530

    fix: correct site site name in navbar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import React, { ComponentPropsWithoutRef } from 'react'
 const Header = (props: ComponentPropsWithoutRef<"header">) => {
     return (
         <header className="p-4 flex justify-between " {...props}>
-            <h3 className="text-3xl text-white hover:cursor-pointer">CODEX BUDDY</h3>
+            <h3 className="text-3xl text-white hover:cursor-pointer">AI BUDDY</h3>
             <span
                 className="hover:cursor-pointer rounded-md bg-stone-600">
                 <a
