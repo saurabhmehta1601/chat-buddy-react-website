e565ab542b0de45c045d620038b104aea51c228a
saurabh <saurabhmehta1601@gmail.com>
2023-01-10 17:15:25 +0530

    feat: add Message component to show chat messages

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -1,8 +1,19 @@
 import React, { ComponentPropsWithoutRef } from 'react'
 
-const Message = (props: ComponentPropsWithoutRef<"header">) => {
+type propTypes = ComponentPropsWithoutRef<"header"> & { text: string, sender: "bot" | "user" }
+
+const Message = (props: propTypes) => {
     return (
-        <>Message</>
+        <article className='flex items-start gap-x-4  p-2 rounded-md'>
+            <img
+                width={32} height={32}
+                src={props.sender === "bot" ? "/assets/bot.png" : "/assets/user.png"}
+            />
+            <div
+                className={" rounded-sm flex-1 " + props.sender === "user" ? "text-right" : "text-left"}>
+                {props.text}
+            </div>
+        </article>
     )
 }
 
