1d7ae07338b3f303de67d1afddecf21ac6c6c44e
saurabh <saurabhmehta1601@gmail.com>
2023-03-05 15:02:54 +0530

    feat: replace express backend with AWS Lambda backend

---
+++ AWS-lambda-backend/index.mjs
@@ -0,0 +1,41 @@
+import { Configuration, OpenAIApi } from "openai";
+import {v4 as uuid} from "uuid";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export async function handler(event) {
+  try {
+    if (!event.prompt) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: { message: "No prompt provided" } }),
+      };
+    }
+    const autoCompleteResponse = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: event.prompt,
+      temperature: 0.2,
+      max_tokens: 1000,
+      presence_penalty: 0.1,
+      frequency_penalty: 0.1,
+    });
+
+    const topChoice = autoCompleteResponse.data.choices[0];
+    const autoCompleteId = uuid();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        id: autoCompleteId,
+        autoComplete: topChoice.text,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: { message: error.message } }),
+    };
+  }
+}
